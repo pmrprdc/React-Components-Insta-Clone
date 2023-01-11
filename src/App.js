801cfd6094dd0e98d164d04cc9f836a0cb916bc9
Pamir Perdeci <pamirperdeci@gmail.com>
2023-01-11 01:49:42 -0500

    Added SearchBar and Posts components inside App Component

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ const newPosts = posts.map((post)=>{
 
   return (
     <div className='App'>
+    <SearchBar />
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>

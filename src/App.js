285f649e8b8b275ea4ef391a58429b643f9f2afb
Pamir Perdeci <pamirperdeci@gmail.com>
2023-01-11 01:48:02 -0500

    configured likePost function to return new state

--- src/App.js
+++ src/App.js
@@ -21,6 +21,15 @@ const App = () => {
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
   const likePost = postId => {
+
+const newPosts = posts.map((post)=>{
+  if(post.id === postId){
+    return {...post, likes:post.likes+1 }
+  }else {
+    return post;
+  }
+  setPosts(newPosts)
+})
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
